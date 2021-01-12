23a776ffa50d7fc68281de976ba43c1f2662a198
задача [#1] <sanya.gorbachewsky@gmail.com>
2021-01-12 22:13:23 +0300

    0. Что такое переменная.[#253306]

---
+++ src/ru/job4j/calculate/variables.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculate;
+
+public class variables {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+        int age = 18;
+        System.out.println(age);
+        long money = 900500;
+        System.out.println(money);
+        float lenght = 78.0332F;
+        System.out.println(lenght);
+        double distance = 100500.99F;
+        System.out.println(distance);
+        boolean baby = true;
+        System.out.println(baby);
+        char gender = 'Y';
+        System.out.println(gender);
+}
+}
