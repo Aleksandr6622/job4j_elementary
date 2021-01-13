8cb6697b35369f4acb5426381fdc1eee38ead50c
задача [#1] <sanya.gorbachewsky@gmail.com>
2021-01-13 21:49:18 +0300

    1. Определите тип данных для переменных.[#253297]

--- src/ru/job4j/calculate/Datatype.java
+++ src/ru/job4j/calculate/Datatype.java
@@ -6,16 +6,16 @@ public class Datatype {
         System.out.println(age);
         char gender = 'm';
         System.out.println(gender);
-        float growth = 170;
-        System.out.println(growth);
-        short brick = 32000;
-        System.out.println(brick);
-        long bacteria =10000;
-        System.out.println(bacteria);
-        double distance = 384400f;
-        System.out.println(distance);
-        boolean resolution = true;
-        System.out.println(resolution);
+        float mansheight = 170;
+        System.out.println(mansheight);
+        short redbrick = 32000;
+        System.out.println(redbrick);
+        long numberofbacteria =10000;
+        System.out.println(numberofbacteria);
+        double distancetothemoon = 384400f;
+        System.out.println(distancetothemoon);
+        boolean workpermit = true;
+        System.out.println(workpermit);
         byte mem = 126;
         System.out.println(mem);
 
