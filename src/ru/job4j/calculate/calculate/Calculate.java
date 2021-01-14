081b8494b0a39714ace4f2dedadef8865c0da877
задача [#1] <sanya.gorbachewsky@gmail.com>
2021-01-14 21:58:12 +0300

    3.1. Переназначение переменной.[#253301]

---
+++ src/ru/job4j/calculate/calculate/Calculate.java
@@ -0,0 +1,10 @@
+package ru.job4j.calculate.calculate;
+
+public class Calculate {
+    public static void main(String[] args) {
+        int size = 10;
+        size = 100;
+        int sizeminusfive = size - 5;
+        System.out.println(sizeminusfive);
+    }
+}
