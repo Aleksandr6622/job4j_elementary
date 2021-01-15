4f9c7febe96742195371cac9d0e1c8473a1df958
задача [#1] <sanya.gorbachewsky@gmail.com>
2021-01-15 21:18:56 +0300

    3.1. Переназначение переменной.[#253301]

--- src/ru/job4j/calculate/Calculate.java
+++ src/ru/job4j/calculate/Calculate.java
@@ -1,20 +1,14 @@
 package ru.job4j.calculate;
 
 public class Calculate {
-    public static void main (String[]args){
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int two = 2;
-        int sixDivtwo = six / two;
-        int fourminustwo = four - two;
-        int fivetimetwo = five * two;
-        System.out.println(sixDivtwo);
-        System.out.println(fourminustwo);
-        System.out.println(fivetimetwo);
+    public static void main (String[]args) {
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
 
 
 
     }
-
 }
+
