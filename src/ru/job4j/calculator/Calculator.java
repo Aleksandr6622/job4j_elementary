7206b18ad174c3bc5cf5ddcf07ce469e9137b15a
задача [#1] <sanya.gorbachewsky@gmail.com>
2021-01-15 21:15:55 +0300

    3. Арифметические операции.[#253304]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,13 +1,19 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-        public static void main (String[]args) {
-            int size = 10;
-            size = 100;
-            size = size - 5;
-            System.out.println(size);
+    public static void main (String[]args){
+        int six =  6 ;
+        int four=  4 ;
+        int five =  5 ;
+        int two =  2 ;
+        int sixDivtwo = six / two;
+        int fourminustwo = four - two;
+        int fivetimetwo = five * two;
+        System.out.println(sixDivtwo);
+        System.out.println(fourminustwo);
+        System.out.println(fivetimetwo);
 
+    }
 
-
-        }
 }
+
